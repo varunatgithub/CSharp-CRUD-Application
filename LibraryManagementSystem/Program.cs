252d1cc8b6a7d34c8336da515913ca1eb25f1261
Varun Reddy <varun13.vr@icloud.com>
2023-09-03 23:51:08 +0530

    Created a Dotnet Console Application

---
+++ LibraryManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Hello, World! welcome to Library Management System.");
+
+
+    }
+  }
+}
